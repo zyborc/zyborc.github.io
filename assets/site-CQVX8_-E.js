88617a567ec2064eae8eb22ceec5a5aa9e95f066
zyborc <zyborc@users.noreply.github.com>
2026-03-07 21:15:59 +0000

    deploy: 63e2d85036230c14acae1524309d582d0e4dc2d1

---
+++ assets/site-CQVX8_-E.js
@@ -0,0 +1,1 @@
+const e=t=>`/${t.startsWith("/")?t.slice(1):t}`;export{e as t};
--- assets/site-dle-0G3c.js
+++
@@ -1,1 +0,0 @@
-const e="/zybotc.github.io/",o=t=>{const s=t.startsWith("/")?t.slice(1):t;return`${e}${s}`};export{o as t};
